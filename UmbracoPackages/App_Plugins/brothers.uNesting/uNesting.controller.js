259c1de11b9fc3ef100f5cefce59aa8af1ffb47b
Tobias Klika <cee@live.at>
2019-08-29 02:39:51 +0200

    start output of element content in nestedcontent property editor

--- UmbracoPackages/App_Plugins/brothers.uNesting/uNesting.controller.js
+++ UmbracoPackages/App_Plugins/brothers.uNesting/uNesting.controller.js
@@ -1,26 +1,101 @@
 ﻿
-angular.module("umbraco").controller("brothers.uNesting.PropertyEditorController", function ($scope, $controller)
+angular.module("umbraco").controller("brothers.uNesting.PropertyEditorController", function ($scope, $controller, $http)
 {
   angular.extend(this, $controller('Umbraco.PropertyEditors.NestedContent.PropertyEditorController', { $scope: $scope }));
 
-  $scope.getView = function (node, idx)
+  var elementsConfig = {};
+
+  var init = function ()
   {
-    if ($scope.model.value[idx])
+    $http.get("/App_Plugins/brothers.uNesting/elements.json").then(function (res)
     {
-      var contentType = $scope.getContentTypeConfig($scope.model.value[idx].ncContentTypeAlias);
+      elementsConfig = res.data;
 
-      if (contentType !== null && contentType.nameExp)
+      _.each($scope.nodes, function (node, idx)
       {
-        // Run the expression against the stored dictionary value, NOT the node object
-        var item = $scope.model.value[idx];
-        var newName = contentType.nameExp(item);
+        node.uNestingContent = getContent(node, $scope.model.value[idx], elementsConfig[node.contentTypeAlias]);
+      });
+    });
+  };
 
-        if (newName && (newName = $.trim(newName)))
+  $scope.$watch("inited", function (newVal)
+  {
+    if (newVal)
+    {
+      init();
+    }
+  });
+
+  var getContent = function (node, item, config)
+  {
+    if (!item)
+    {
+      return '';
+    }
+    if (!config)
+    {
+      return node.documentType.description;
+    }
+
+    var lines = [];
+
+    _.each(config, function (type, alias)
+    {
+      var value = item[alias];
+      if (value)
+      {
+        if (type === 'text')
         {
-          return newName;
+          lines.push(stripHtml(value));
+        }
+        if (type === 'url')
+        {
+          lines.push(value[0].url);
         }
       }
+    });
+
+    if (lines.length < 1)
+    {
+      return node.documentType.description;
     }
-    return node.documentType.description || 'Enter data ...';
+    else
+    {
+      return lines.join('<br>');
+    }
   };
+
+
+  var stripHtml = function (html)
+  {
+    if (!html)
+    {
+      return '';
+    }
+
+    var stripped = html.replace('<br>', ' ').replace('<br />', ' ').replace('<br/>', ' ').replace(/<[^>]+>/gm, '');
+
+    return stripped.length > 120 ? (stripped.substring(0, 120) + '...') : stripped;
+  };
+
+  //$scope.getView = function (node, idx)
+  //{
+  //  if ($scope.model.value[idx])
+  //  {
+  //    var contentType = $scope.getContentTypeConfig($scope.model.value[idx].ncContentTypeAlias);
+
+  //    if (contentType !== null && contentType.nameExp)
+  //    {
+  //      // Run the expression against the stored dictionary value, NOT the node object
+  //      var item = $scope.model.value[idx];
+  //      var newName = contentType.nameExp(item);
+
+  //      if (newName && (newName = $.trim(newName)))
+  //      {
+  //        return newName;
+  //      }
+  //    }
+  //  }
+  //  return node.documentType.description || 'Enter data ...';
+  //};
 });
