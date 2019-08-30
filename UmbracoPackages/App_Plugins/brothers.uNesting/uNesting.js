26f587766d0923a8d2bc3acb25707a172b7b4e41
Tobias Klika <cee@live.at>
2019-08-30 15:40:22 +0200

    add un-text, un-boxes & un-figure directives

--- UmbracoPackages/App_Plugins/brothers.uNesting/uNesting.js
+++ UmbracoPackages/App_Plugins/brothers.uNesting/uNesting.js
@@ -108,49 +108,69 @@ angular.module('umbraco.directives').directive('unContent', function ($compile)
 
 angular.module('umbraco.directives').directive('unMedia', function ($http, $compile, umbRequestHelper)
 {
+  var sizes = {
+    small: 'width=30&height=30&mode=crop&quality=60',
+    medium: 'width=60&height=60&mode=crop&quality=60',
+    large: 'width=100&height=100&mode=crop&quality=60',
+    wide: 'width=100&height=60&mode=crop&quality=60',
+    high: 'width=60&height=100&mode=crop&quality=60'
+  };
+
   return {
     restrict: 'E',
     scope: {
-      items: '=',
+      model: '=',
       key: '@',
       titleKey: '@',
-      limit: '@'
+      limit: '@',
+      size: '@',
+      hideLabel: '@'
     },
-    template: '' +
-    '<div class="unesting-media">' +
-      '<div class="unesting-media-item" ng-repeat="item in media" ng-class="{\'has-title\': !!item.title}">' +
-        '<img src="{{item.src}}" title="{{item.title}}" class="unesting-media-item-image" />' +
-        '<span class="unesting-media-item-text" ng-if="item.title">{{item.title | unHtml }}</span>' +
-      '</div>' +
-      '<span class="unesting-media-more" ng-if="count > media.length">+{{count - media.length}}</span>' +
-    '</div>',
+
+    template: '<div class="unesting-media" unesting-size="{{_size}}">' +
+                '<div class="unesting-media-item" ng-repeat="item in media" ng-class="{\'has-title\': !!item.title}">' +
+                  '<img src="{{item.src}}" title="{{item.title}}" class="unesting-media-item-image" />' +
+                  '<span class="unesting-media-item-text" ng-if="item.title">{{item.title | unHtml }}</span>' +
+                '</div>' +
+                '<span class="unesting-media-more" ng-if="count > media.length">+{{count - media.length}}</span>' +
+              '</div>',
+
     link: function (scope, element)
     {
-      var resize = 'width=50&height=50&mode=crop&quality=60';
+      scope._size = sizes[scope.size] ? scope.size : 'medium';
+
+      var resize = sizes[scope._size];
       var limit = scope.limit || 5;
       scope.media = [];
 
       var titleCache = {};
 
+
       // compile & render template
       function render()
       {
-        var ids = scope.items;
+        var ids = scope.model;
 
-        if (!scope.items)
+        if (!scope.model)
         {
           return;
         }
-        if (_.isArray(scope.items))
+
+        if (_.isArray(scope.model))
         {
-          if (!scope.key)
+          if (typeof scope.model[0] !== 'string' && !scope.key)
           {
             console.error("You need the <un-media key='my_key' /> property for nested media");
             return;
           }
 
-          ids = _.map(scope.items, function (item)
+          ids = _.map(scope.model, function (item)
           {
+            if (typeof item === 'string')
+            {
+              return item;
+            }
+
             var value = item[scope.key];
 
             if (scope.titleKey)
@@ -162,7 +182,7 @@ angular.module('umbraco.directives').directive('unMedia', function ($http, $comp
           }).join(',');
         }
 
-        ids = ids.split(',');
+        ids = _.filter(ids.split(','), function (id) { return !!id; });
         scope.count = ids.length;
 
         var query = "ids=" + ids.slice(0, limit).join('&ids=');
@@ -178,10 +198,216 @@ angular.module('umbraco.directives').directive('unMedia', function ($http, $comp
               title: titleCache[id]
             });
           });
+
+          // hide label if requested
+          if (scope.media.length > 0 && (scope.hideLabel === true || scope.hideLabel === 'true' || scope.hideLabel === '1'))
+          {
+            element.closest('.unesting-item-header').addClass('no-label');
+          }
         });
       }
 
       render();
+    }
+  };
+});
+
+
+
+angular.module('umbraco.directives').directive('unText', function ($filter)
+{
+  return {
+    restrict: 'E',
+    scope: {
+      model: '=',
+      maxLines: '@',
+      hideLabel: '@'
+    },
+
+    template: '<div class="unesting-text" ng-if="items.length" ng-style="{ \'-webkit-line-clamp\': {{maxLines}}, \'max-height\': \'{{maxLines * 20}}px\' }">' +
+                '<span class="unesting-text-partial" unesting-index="{{$index}}" ng-repeat="text in items" ng-bind-html="text"></span>' + 
+              '</div>',
+
+    link: function (scope, element)
+    {
+      scope.maxLines = +scope.maxLines || 2;
+      scope.items = [];
+
+      if (!scope.model)
+      {
+        return;
+      }
+
+      // hide label if requested
+      if (scope.hideLabel === true || scope.hideLabel === 'true' || scope.hideLabel === '1')
+      {
+        element.closest('.unesting-item-header').addClass('no-label');
+      }
+
+      var partials = _.filter(!_.isArray(scope.model) ? [scope.model] : scope.model, function (text) { return !!text; });
+
+      scope.items = _.map(partials, function (text)
+      {
+        return $filter('unHtml')(text, 1000);
+      });
+    }
+  };
+});
+
+
+
+angular.module('umbraco.directives').directive('unBoxes', function ($compile)
+{
+  return {
+    restrict: 'E',
+    scope: {
+      model: '=',
+      key: '@',
+      titleKey: '@',
+      limit: '@',
+      size: '@',
+      hideLabel: '@'
+    },
+
+    template: '<div class="unesting-boxes" unesting-size="{{size}}">' +
+                '<div class="unesting-boxes-item" ng-repeat="item in items" ng-class="{\'has-title\': !!item.title}">' +
+                  '<span class="unesting-boxes-item-headline" ng-if="item.headline">{{item.headline | unHtml }}</span>' +
+                  '<span class="unesting-boxes-item-text" ng-if="item.text" ng-style="{ \'max-height\': \'{{item.maxLines * 20}}px\', \'-webkit-line-clamp\': {{item.maxLines}} }">{{item.text | unHtml:200 }}</span>' +
+                '</div>' +
+                '<span class="unesting-boxes-more" ng-if="count > items.length">+{{count - items.length}}</span>' +
+              '</div>',
+
+    link: function (scope, element)
+    {
+      var maxLines = {
+        medium: 3,
+        large: 6
+      };
+
+      scope.size = maxLines[scope.size] ? scope.size : 'medium';
+      var limit = +scope.limit || 3;
+      scope.items = [];
+
+      if (!scope.model)
+      {
+        return;
+      }
+
+      // hide label if requested
+      if (scope.hideLabel === true || scope.hideLabel === 'true' || scope.hideLabel === '1')
+      {
+        element.closest('.unesting-item-header').addClass('no-label');
+      }
+
+      if (!_.isArray(scope.model))
+      {
+        console.error("The <un-boxes model='my_array' /> model has to be an array");
+        return;
+      }
+
+      if (_.isObject(scope.model[0]) && !scope.key)
+      {
+        console.error("You need the <un-boxes key='my_key' /> property for nested elements");
+        return;
+      }
+
+      var items = [];
+      _.each(scope.model, function (item)
+      {
+        var max = maxLines[scope.size];
+
+        if (item)
+        {
+          if (typeof item === 'string')
+          {
+            items.push({
+              maxLines: max,
+              text: item
+            });
+          }
+
+          var headline = item[scope.titleKey];
+
+          items.push({
+            maxLines: headline ? max - 1 : max,
+            headline: headline,
+            text: item[scope.key]
+          });
+        }
+      });
+
+      scope.items = items.slice(0, limit);
+      scope.count = items.length;
+    }
+  };
+});
+
+
+
+angular.module('umbraco.directives').directive('unFigure', function ($filter)
+{
+  return {
+    restrict: 'E',
+    scope: {
+      image: '=',
+      text: '=',
+      headline: '=',
+      size: '@',
+      maxLines: '@',
+      hideLabel: '@'
+    },
+
+    template: '<div class="unesting-figure" ng-if="hasContent">' +
+                '<un-media ng-if="image" model="image" size="{{size}}" />' +
+                '<div class="unesting-figure-content">' +
+                  '<span class="unesting-figure-headline" ng-if="headline">{{headline | unHtml }}</span>' +
+                  '<span class="unesting-figure-text" ng-if="text" ng-style="{ \'max-height\': \'{{maxLines * 20}}px\', \'-webkit-line-clamp\': {{maxLines}} }">{{text | unHtml:800 }}</span>' +
+                '</div>' +
+              '</div>',
+
+    link: function (scope, element)
+    {
+      scope.hasContent = !!(scope.text || scope.headline || scope.image);
+
+      // hide label if requested
+      if (scope.hideLabel === true || scope.hideLabel === 'true' || scope.hideLabel === '1')
+      {
+        element.closest('.unesting-item-header').toggleClass('no-label', scope.hasContent);
+      }
+
+      // calculate max lines
+      scope.maxLines = +scope.maxLines || 3;
+      if (scope.headline)
+      {
+        scope.maxLines -= 1;
+      }
+    }
+  };
+});
+
+
+
+angular.module('umbraco.directives').directive('unConfig', function ($filter)
+{
+  return {
+    restrict: 'E',
+    scope: {
+      hideLabel: '@',
+      asColumns: '@'
+    },
+
+    template: '',
+
+    link: function (scope, element)
+    {
+      if (scope.hideLabel === true || scope.hideLabel === 'true' || scope.hideLabel === '1')
+      {
+        element.closest('.unesting-item-header').addClass('no-label');
+      }
+      if (scope.asColumns === true || scope.asColumns === 'true' || scope.asColumns === '1')
+      {
+        element.closest('.unesting-item-header').addClass('as-columns');
+      }
     }
   };
 });
@@ -201,7 +427,7 @@ angular.module('umbraco').filter('unHtml', function ()
       maxLength = 120;
     }
 
-    var stripped = html.replace('<br>', ' ').replace('<br />', ' ').replace('<br/>', ' ').replace(/<[^>]+>/gm, '');
+    var stripped = html.replace('<br>', ' ').replace('<br />', ' ').replace('<br/>', ' ').replace(/<[^>]+>/gm, ' ');
 
     return stripped.length > maxLength ? (stripped.substring(0, maxLength) + '...') : stripped;
   };
